db5cdd510e2c3df5754aca07467717119d37f9f1
0strezz <juan.dpmz05@gmail.com>
2026-03-09 22:52:38 -0500

    casos de prueba para loadAbook

--- src/test/java/edu/eci/dosw/LibraryTest.java
+++ src/test/java/edu/eci/dosw/LibraryTest.java
@@ -6,4 +6,18 @@ public class LibraryTest {
 		Loan loan = library.loanABook("999", "1234567890");
 		AssertEquals(null, loan);
 	}
+
+	@Test
+	void loanABookShouldReturnNullIfBookDoesNotExist() {
+    	Loan loan = library.loanABook("1", "0000000000");
+    	assertEquals(null, loan);
+	}
+
+	@Test
+	void loanABookShouldNotAllowSameUserToBorrowSameBookTwice() {
+		Loan firstLoan = library.loanABook("1", "0987654321");
+		Loan secondLoan = library.loanABook("1", "0987654321");
+		assertNotNull(firstLoan);
+		assertEquals(null, secondLoan);
+	}
 }
